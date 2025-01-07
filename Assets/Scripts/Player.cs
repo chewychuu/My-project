5cb8b3e4020bf2ffda21be91c78984843ac00977
default <gabija.godlauskaite@vilnius.eu>
2025-01-07 19:37:38 +0200

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private float speed = 5000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        else if
+        (Input.GetKey(KeyCode.A)) { 
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
